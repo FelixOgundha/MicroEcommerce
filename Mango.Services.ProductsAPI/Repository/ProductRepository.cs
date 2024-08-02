4740d0cf152c5c9654ce147f3679e352ad498cdd
FelixOgundha <fogundha@strathmore.edu>
2024-08-02 18:48:20 +0300

    updated configs

---
+++ Mango.Services.ProductsAPI/Repository/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Mango.Services.ProductsAPI.DataDbContext;
+using Mango.Services.ProductsAPI.Models;
+using Mango.Services.ProductsAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductsAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext db,IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            var product = await _db.Products.FirstOrDefaultAsync(q => q.ProductId == productDto.ProductId);
+            var updatedProduct = _mapper.Map<ProductDto, Product>(productDto);
+
+            if (product == null){
+                _db.Products.Add(updatedProduct);
+            }
+
+            _db.Products.Update(updatedProduct);
+
+            await _db.SaveChangesAsync();
+
+            return productDto;
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            try
+            {
+                var product = await _db.Products.FirstOrDefaultAsync(q => q.ProductId == id);
+
+                if (product == null) { return false; }
+
+                _db.Remove(product);
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<ProductDto> GetProductById(int id)
+        {
+            var product = await _db.Products.FirstOrDefaultAsync(q => q.ProductId == id);
+
+            return _mapper.Map<Product,ProductDto>(product);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            IEnumerable<Product> productsList = await _db.Products.ToListAsync();
+
+            var result = _mapper.Map<IEnumerable<ProductDto>>(productsList);
+
+            return result;
+        }
+    }
+}
