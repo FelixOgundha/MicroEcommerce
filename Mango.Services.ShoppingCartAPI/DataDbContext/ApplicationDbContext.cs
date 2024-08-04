4418df17e0ed7a74aadcd79ca385b026c885d9aa
winnieamandela@gmail.com <83855798+FelixOgundha@users.noreply.github.com>
2024-08-04 18:27:17 +0300

    updated microecommerce

---
+++ Mango.Services.ShoppingCartAPI/DataDbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.DataDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+       
+    }
+}
